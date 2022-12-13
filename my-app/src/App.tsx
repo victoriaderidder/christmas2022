0b4ee76e93dd16acf5b2ea68d158b589d1677600
Victoria Fisher <vleighfisher@gmail.com>
2022-12-12 22:53:50 -0500

    changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,13 +4,7 @@ import Travel from './components/TheJourney';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className='content'>
-          <Travel />
-        </div>
-      </header>
-    </div>
+    <Travel />
   );
 }
 
--- my-app/src/components/TheJourney.tsx
+++ my-app/src/components/TheJourney.tsx
@@ -1,31 +1,52 @@
 import { useState } from "react";
 import Title from "./Title";
 import snowflake from '../assets/img/snowflake.png'
-import song from '../assets/music/Krampus.mp3'
+import Krampus from '../assets/music/Krampus.mp3'
+import DeckTheHalls from '../assets/music/DeckTheHalls.mp3'
 import '../styles/TheJourney.css'
 
-const story = [
-    <img src={snowflake} id='snowflake' alt='snowflake' onClick={() => playSong()} />,
-    <Title title='> Lizzy.' />,
-    'You wake up.',
-    'this',
-    'game'
-];
-
-const playSong = () => {
-    const audio = new Audio(song);
-    audio.loop = true;
-    audio.play()
-};
-
-export default function Travel() {
+export default function Travel(this: any) {
+    const [audio, setAudio] = useState(new Audio(Krampus));
+    const [playing, setPlaying] = useState(false);
     const [index, setIndex] = useState(Number);
-    const increment = () => {
-      index < 3 && setIndex(index + 1);
+    const [bgColor, setBgColor] = useState('#282c34');
+
+    const playSong = (song: any) => {
+        //audio.pause()
+        setAudio(new Audio(song))
+        audio.loop = true;
+        audio.play()
     };
+
+    const playAndChange = ( song: string, color: string) => {
+        playSong(song);
+        setBgColor(color);
+    }
+
+    const increment = () => {
+      //index < 3 && 
+      setIndex(index + 1);
+    };
+
+    const story = [
+        //onClick={() => playSong(Krampus)}
+        <img src={snowflake} id='snowflake' alt='snowflake' onClick={() => playSong(DeckTheHalls)} />,
+        <Title title='> Lizzy.' />,
+        'You wake up.',
+        'This is not the North Pole.',
+        'Not anymore.',
+        <><div onClick={() => playAndChange(DeckTheHalls, '#C30F16')}><Title title='> Elfward.' /></div></>,
+        <><div onClick={() => playAndChange(Krampus, '#282c34')}>Yay! You're at the North Pole!!!</div></>,
+        'Except Santa is missing and Christmas is ruined and everything is absolutely terrible.'
+    ];
+    
     return (
-    <div className="Journey" onClick={increment}>
-        {story[index]}
-    </div>
+        <div className="App" style={{ backgroundColor: bgColor }}>
+            <header className="App-header">
+                <div className="journey" onClick={increment} style={{ backgroundColor: bgColor }}>
+                    {story[index]}
+                </div>
+            </header>
+     </div>
  );
 }
