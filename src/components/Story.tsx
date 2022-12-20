e29d7a0b18a7a3b02673d7f25bbbf29cbc5d566e
Victoria Fisher <vleighfisher@gmail.com>
2022-12-19 23:32:51 -0500

    add fontawesome

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,14 +1,28 @@
+import { Button, darken } from '@mui/material';
 import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 interface StoryProps {
   story: string;
+  bgColor?: boolean
 }
 
-const Story: FC<StoryProps> = ({ story }) => {
+const Story: FC<StoryProps> = ({ story, bgColor }) => {
   return (
     <>
       {story}
-      <div>{'==>'}</div>
+      <div>        
+        <Button
+            size="small"
+            variant="contained"
+            style={{
+                color: "white",
+                backgroundColor: bgColor ? '#C30F16' : 'rgb(40, 44, 52)',
+            }}>
+            <FontAwesomeIcon icon={faArrowRightLong} size="lg" />
+        </Button>
+        </div>
     </>
   );
 };
